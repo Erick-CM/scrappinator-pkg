8987f9840a0550b8c672965e7cd74cd6932f104d
Erick-CM <erick.fcm.0@gmail.com>
2024-09-17 15:40:30 -0600

    add some functions

--- src/classes/Scrappinator.ts
+++ src/classes/Scrappinator.ts
@@ -1,4 +1,4 @@
-import { Goto } from '../interfaces/scrapper';
+// import { Goto } from '../interfaces/scrapper';
 
 // interface ScrapperPage {
 //     goto<T extends Goto>(params: T): void;
@@ -9,6 +9,9 @@ export interface ScrapperLib {
     version: string;
     // page: Partial<ScrapperPage> | null;
     // config: any; // ConfigOptions interface
+    // goto<T extends Goto>(params: T): void;
+    waitFor(selector: string, options?: any): void;
+    fill(selector: string, value: string, options?: any): void;
 }
 
 export class Scrappinator<T extends ScrapperLib> {
