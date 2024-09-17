7904d73e39940619024903bc280d34fc5bd40b70
Erick-CM <erick.fcm.0@gmail.com>
2024-09-17 09:14:22 -0600

    initial commit

---
+++ scrappinator.d.ts
@@ -0,0 +1,14 @@
+declare module '@scrappinator' {
+    export interface Scrappinator {
+      // Add properties and methods here based on Scrappinator class definition
+    }
+
+    export interface ScrapperLib {
+      // Add properties and methods here based on ScrapperLib interface definition
+    }
+
+    export interface Goto {
+        url: string;
+        // Add other properties and methods of Goto interface
+    }
+}
---
+++ src/classes/Scrappinator.ts
@@ -0,0 +1,20 @@
+import { Goto } from '../interfaces/scrapper';
+
+// interface ScrapperPage {
+//     goto<T extends Goto>(params: T): void;
+// }
+
+export interface ScrapperLib {
+    name: string;
+    version: string;
+    // page: Partial<ScrapperPage> | null;
+    // config: any; // ConfigOptions interface
+}
+
+export class Scrappinator<T extends ScrapperLib> {
+    scrapper: T | null = null;
+
+    constructor(tech: T) {
+        this.scrapper = tech;
+    }
+}
